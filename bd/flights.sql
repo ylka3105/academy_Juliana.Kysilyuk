7ce8cc9addaa424cac44cc30acca1e3beee1d3dd
ylka3105 <ylka3105@gmail.com>
2014-10-26 15:30:28 +0200

    bd

---
+++ bd/flights.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Окт 25 2014 г., 23:42
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `flights`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `airlines`
+--
+
+CREATE TABLE IF NOT EXISTS `airlines` (
+  `airline_id` int(7) NOT NULL,
+  `name` text NOT NULL,
+  `year` year(4) NOT NULL,
+  `city_id` int(7) NOT NULL,
+  `adress` text NOT NULL,
+  `person_id` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
